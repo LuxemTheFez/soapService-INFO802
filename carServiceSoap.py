860f445b9a92944879eb9acc93f6fdfa549cd5be
LuxemTheFez <luxem192@gmail.com>
2022-02-10 16:04:25 +0100

    Create carServiceSoap.py

---
+++ carServiceSoap.py
@@ -0,0 +1,52 @@
+from spyne import Application, rpc, ServiceBase, Iterable, Integer, Unicode, AnyDict
+from spyne.protocol.soap import Soap11
+from spyne.server.wsgi import WsgiApplication
+from wsgiref.simple_server import make_server
+from spyne.protocol.json import JsonDocument
+import json
+
+carStat = [
+        {
+            'model': 'Dacia spring Electric',
+            'range' : 170,
+            'fastChargeTime' : 40,
+            'chargeTime' :  300
+        },
+        {
+            'model': 'Renault Twingo Electric',
+            'range' : 130,
+            'fastChargeTime' : 0,
+            'chargeTime' :  75
+        },
+        {
+            'model': 'Fiat 500e Hatchback',
+            'range' : 135,
+            'fastChargeTime' : 24,
+            'chargeTime' :  150
+        },
+        {
+            'model': 'Tesla Model 3 Performance',
+            'range' : 470,
+            'fastChargeTime' : 25,
+            'chargeTime' :  495
+        }
+        ]
+class carService(ServiceBase):
+    @rpc(_returns=Unicode)
+    def get_car(ctx):
+        return json.dumps(carStat)   
+
+    
+application = Application([carService], 'spyne.examples.hello.soap',
+                            in_protocol=Soap11(validator='lxml'),
+                            out_protocol=Soap11())
+wsgi_application = WsgiApplication(application)
+
+
+def main():
+    server = make_server('localhost', 3000, wsgi_application)
+
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
